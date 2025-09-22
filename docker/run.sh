e0137b87a8ff2b6986cd00208fd239127cecc0a4
Charles <charlesthomasomer@gmail.com>
2025-09-23 00:04:42 +0100

    Fixes and info improvements

--- docker/run.sh
+++ docker/run.sh
@@ -3,6 +3,8 @@
 # exist if any command returns a non-zero result
 set -e
 
+echo "Shairport Sync Startup ($(date))"
+
 if [ -z ${ENABLE_AVAHI+x} ] || [ $ENABLE_AVAHI -eq 0 ]; then
   rm -rf /run/dbus/dbus.pid
   rm -rf /run/avahi-daemon/pid
@@ -11,13 +13,15 @@ if [ -z ${ENABLE_AVAHI+x} ] || [ $ENABLE_AVAHI -eq 0 ]; then
   dbus-daemon --system
 fi
 
+echo "Starting NQPTP ($(date))"
+
 (/usr/local/bin/nqptp > /dev/null 2>&1) &
 
 [ -z ${ENABLE_AVAHI+x} ] || [ $ENABLE_AVAHI -eq 0 ] || avahi-daemon --daemonize --no-chroot
 
 while [ ! -f /var/run/avahi-daemon/pid ]; do
-  echo "Warning: avahi is not running, sleeping for 1 second before trying to start shairport-sync"
-  sleep 1
+  echo "Warning: avahi is not running, sleeping for 5 seconds before trying to start shairport-sync"
+  sleep 5
 done
 
 # for PipeWire
@@ -26,5 +30,7 @@ export XDG_RUNTIME_DIR=/tmp
 # for PulseAudio
 export PULSE_SERVER=unix:/tmp/pulseaudio.socket
 export PULSE_COOKIE=/tmp/pulseaudio.cookie
+
+echo "Finished startup tasks ($(date)), starting Shairport Sync."
 
 exec /usr/local/bin/shairport-sync "$@"
